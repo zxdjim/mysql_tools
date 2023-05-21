8390498ad936eeef59590af4ec2fd37344b5ccfd
zxdjim <50045581+zxdjim@users.noreply.github.com>
2023-05-21 14:40:10 +0800

    Update unlink_redis.sh

--- unlink_redis.sh
+++ unlink_redis.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 ## v1 kaiens 20220524
 ##执行前需要调整一下默认的端口和密码；
-##传递一个参数：需要删除的key前缀字符
+##传递一个参数：需要删除的key
 ## v2 baider 20220526
 ## 说明 $1(ip:port)参数可获取任一redis集群的信息
 ##      $2(-1:all 0:slave 1:master) 参数指定要在哪些节点执行脚本
-##      $3(要执行unlink的模糊key,后模糊)   
+##      $3(要执行unlink的key)   
+##      $4(如果是unlink的key删除，此值说明是带双引号的key 0:否 1:是)
+## sh unlink_redis.sh 127.0.0.1:7400 -1 '\"abc*\"' 1
+## sh unlink_redis.sh 127.0.0.1:7400 -1 abc* 0
 
 v_redis="/usr/bin/redis-cli"
-token="yD1fVziT4svT4rtKzuN1"
+token="xxxyyy"
 ip=${1%:*}
 port=${1#*:}
-delk=$3"*"
+
 
 error()
 {
@@ -51,6 +54,10 @@ do
    v_ip=${host_port%:*}
    v_port=${host_port#*:}
    echo -e "\033[34;40m${host_port}\033[0m"
-   redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token --scan --pattern "$delk" |xargs -i redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token unlink {}
+   if [ $4 == "1" ]; then
+     redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token --scan --pattern "'${3}'" |sed 's/.$//'|sed 's/^/\\/'|sed 's/.$//'|sed 's/$/\\"/'|xargs -i redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token unlink {}
+   else
+     redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token --scan --pattern ${3} |xargs -i redis-cli -h ${v_ip} -p ${v_port} --no-auth-warning -a $token unlink {}
+   fi
 done
 echo -e "\033[34;40m所有节点unlink执行完毕!!!\033[0m"
